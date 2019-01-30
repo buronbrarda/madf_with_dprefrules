60d20d5297bc4a7e6bc199ae6c8f836be21e8d0b
Martin Buron <martin_buron@cs.uns.edu.ar>
2019-01-30 15:43:26 -0300

    StepsView Panel Design

--- java_ui/src/java_ui/DSJavaUI.java
+++ java_ui/src/java_ui/DSJavaUI.java
@@ -1,10 +1,48 @@
 package java_ui;
 
-public class DSJavaUI {
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+public class DSJavaUI{
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DSJavaUI window = new DSJavaUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
+	/**
+	 * Create the application.
+	 */
+	public DSJavaUI() {
+		initialize();
+		
+		JPanel stepsView = new StepsView();
+		frame.getContentPane().add(stepsView, BorderLayout.CENTER);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
